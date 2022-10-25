27025da56c8eb1ce1ede58e7c2bf6fbfd7f231d5
OcdMonster <83742772+OcdMonster@users.noreply.github.com>
2022-10-25 15:05:56 -0500

    Add files via upload

---
+++ session-13/index.js
@@ -0,0 +1,21 @@
+//Duplica el archivo del ejercicio de la sesión 04-Textos
+
+let nombre =   "Max"
+let apellido = "Curiel  "
+let estudiante = nombre + " " + apellido
+let estudianteMayus = estudiante.toUpperCase()
+let estudianteMinus = estudiante.toLowerCase()
+let numeroLetras = estudiante.length
+let first = nombre[2]
+let last = apellido[5]
+let removeSpace = estudiante.trim()
+let contains = estudiante.includes(nombre)
+
+//Utiliza los tres estilos de comillas de forma alterna (comillas simples, dobles, backticks)
+
+let Max = "Max"
+let Monica  = "Monica"
+let Andre = "André"
+
+
+
