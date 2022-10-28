1602722eee4fd6fb324ea6217c5974162b6ccc5c
OcdMonster <83742772+OcdMonster@users.noreply.github.com>
2022-10-27 23:39:43 -0500

    Add files via upload

---
+++ company/HolaMundo.java
@@ -0,0 +1,58 @@
+//Dentro del proyecto tenéis que crear un paquete.
+package com.company;
+
+//En el paquete tendréis que crear una clase.
+public class HolaMundo {
+    //Dentro de la clase tenéis que crear el método main.
+    public static void main(String[] args) {
+        //imprimir todos los datos que se han visto en las sesiones
+
+        System.out.println("HolaMundo");
+
+        //numericos
+        byte var1 = 5;
+        System.out.println(var1);
+
+        short var2 = 10;
+        System.out.println(var2);
+
+        int var3 = 30;
+        System.out.println(var3);
+
+        long var4 = 5530128921L;
+        System.out.println(var4);
+
+        float var5 = 5.5f;
+        System.out.println(var5);
+
+        double var6 = 10.5d;
+        System.out.println(var6);
+
+        var6 = 20.5d;
+        System.out.println(var6);
+
+        double precioSilla = 49.99;
+        System.out.println(precioSilla);
+
+
+        //booleanos
+
+        boolean var7 = true;
+        System.out.println(var7);
+
+        boolean var8 = false;
+        System.out.println(var8);
+
+
+        //texto
+
+        char var9 = 'a';
+        System.out.println(var9);
+
+        String var10 = "Lorem ipsum dolor";
+        System.out.println(var10);
+
+
+
+    }
+}
